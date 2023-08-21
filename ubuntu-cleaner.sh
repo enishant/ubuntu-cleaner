c551e52543bc94f9dfc8a96c96a3bbd841b7215a
Nishant Vaity <enishant@users.noreply.github.com>
2023-08-21 20:42:20 +0530

    Update and rename clean.sh to ubuntu-cleaner.sh

--- ubuntu-cleaner.sh
+++ ubuntu-cleaner.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# References:
-# https://www.esds.co.in/kb/how-to-clean-up-ubuntu-server/
-# https://www.omgubuntu.co.uk/2016/08/5-ways-free-up-space-on-ubuntu
-# https://www.tecmint.com/clear-ram-memory-cache-buffer-and-swap-space-on-linux/
-# https://askubuntu.com/a/1156686
-# https://gist.github.com/Iman/8c4605b2b3ce8226b08a
-
 sudo apt-get autoremove
 sudo apt-get clean
 sudo rm -fr /home/$USER/.cache/thumbnails/*
